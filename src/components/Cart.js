5057b6df01bb6ba5b9906ab23fb613e174a927a9
cagatay <cgtyeo@gmail.com>
2023-10-07 02:25:22 +0300

    style(cart): update styles in Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -12,17 +12,19 @@ const Cart = () => {
         {cartItems.map((item, i) => (
           <li key={item.id}>
             <span>{item.parentObject.OCG[1].MBS}</span>
-            <span>Kod:{item.parentObject.C}</span>
-            <span>Maç:{item.parentObject.N}</span>
-            <span>Oran: {item.itemValue}</span>
+            <span>Kod: {item.parentObject.C}</span>
+            <span>Maç: {item.parentObject.N}</span>
+            <span className="boldText">Oran: {item.itemValue}</span>
           </li>
         ))}
       </ul>
 
-      {cartItems.length == 0 ? (
+      {cartItems.length === 0 ? (
         <span>Toplam Tutar: 0 TL</span>
       ) : (
-        <span>Toplam Tutar: {multiplyOfCartPrices.toFixed(2)} TL</span>
+        <span className="largeText">
+          Toplam Tutar: {multiplyOfCartPrices.toFixed(2)} TL
+        </span>
       )}
     </div>
   );
