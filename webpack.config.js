0a0806235e68bc855df483c5787908c29edfb010
cagatay <cgtyeo@gmail.com>
2023-10-07 04:00:04 +0300

    feat(webpack): css-loader added to config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const config = {
-  entry: "./src/index.js",
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+
   output: {
-    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/assets/index.html",
-    }),
-  ],
-};
-
-module.exports = () => {
-  return config;
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  mode: "development",
 };
