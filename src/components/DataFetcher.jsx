feb907dbc8c693373aa03780db275812a2a524d3
cagatay <cgtyeo@gmail.com>
2023-10-07 16:47:30 +0300

    feat(data-fetching): add caching mechanism inside the fetch

--- src/components/DataFetcher.jsx
+++ src/components/DataFetcher.jsx
@@ -1,5 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useContext, useEffect } from 'react';
 import { DataContext } from '../context/DataContext';
+import { getCachedBets, saveBets } from '../utils/local-storage';
 
 const DataFetcher = () => {
   const { setLoader, setTickets, setError } = useContext(DataContext);
@@ -7,11 +9,18 @@ const DataFetcher = () => {
 
   useEffect(() => {
     const fetchTickets = async () => {
+      const bets = getCachedBets();
+      if (bets) {
+        setTickets(bets);
+        return;
+      }
       setLoader(true);
       try {
         const response = await fetch(URL);
         const data = await response.json();
+
         setTickets(data);
+        saveBets(data);
       } catch (error) {
         setError(error);
       } finally {
